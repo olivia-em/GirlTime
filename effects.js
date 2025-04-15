eb7429d897fea5df429bc2911c6fc61948aa109c
olivia-em <olee0114@gmail.com>
2025-04-14 20:05:07 -0400

    small change

--- effects.js
+++ effects.js
@@ -53,32 +53,32 @@ function setup() {
     Tone.Transport.start();
     // startButton.hide();
   });
-  let stopButton = createButton('Stop Audio');
-  stopButton.position(width/2 - 50, height/2 + 100);
-  stopButton.mousePressed(() => {
-    // Tone.stop();
-    Tone.Transport.stop();
-    // staButton.hide();
-  });
+  // let stopButton = createButton('Stop Audio');
+  // stopButton.position(width/2 - 50, height/2 + 100);
+  // stopButton.mousePressed(() => {
+  //   // Tone.stop();
+  //   Tone.Transport.stop();
+  //   // staButton.hide();
+  // });
 }
 
-function draw() {
-  // let mappedDecay = map(inData[6], 600, 0, 3.0, 0.5);
-  // reverb.decay = mappedDecay;
-  background(240);
-  textAlign(CENTER, CENTER);
-  text("Press keys 1-6 to toggle chords\nFirst key press picks a random root", width/2, height/2);
+// function draw() {
+//   // let mappedDecay = map(inData[6], 600, 0, 3.0, 0.5);
+//   // reverb.decay = mappedDecay;
+//   background(240);
+//   textAlign(CENTER, CENTER);
+//   text("Press keys 1-6 to toggle chords\nFirst key press picks a random root", width/2, height/2);
   
-  // Show active chords
-  let y = height/2 + 20;
-  text("Active chords: " + Object.keys(activeChords).join(", "), width/2, y);
+//   // Show active chords
+//   let y = height/2 + 20;
+//   text("Active chords: " + Object.keys(activeChords).join(", "), width/2, y);
   
-  // Show root note if set
-  if (rootNote) {
-    y += 20;
-    text("Root: " + Tone.Frequency(rootNote, "midi").toNote(), width/2, y);
-  }
-}
+//   // Show root note if set
+//   if (rootNote) {
+//     y += 20;
+//     text("Root: " + Tone.Frequency(rootNote, "midi").toNote(), width/2, y);
+//   }
+// }
 
 function keyPressed() {
   if (key >= '1' && key <= '6') {
@@ -206,78 +206,78 @@ function getNote(scalePos) {
 }
 
 
-function allSerialStuff() {
-  if (!navigator.serial) {
-    alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
-  }
-  // check for any ports that are available:
-  serial.getPorts();
-  // if there's no port chosen, choose one:
-  serial.on("noport", makePortButton);
-  // open whatever port is available:
-  serial.on("portavailable", openPort);
-  // handle serial errors:
-  serial.on("requesterror", portError);
-  // handle any incoming serial data:
-  serial.on("data", serialEvent);
-  serial.on("close", makePortButton);
-  // add serial connect/disconnect listeners:
-  navigator.serial.addEventListener("connect", portConnect);
-  navigator.serial.addEventListener("disconnect", portDisconnect);
-}
-// if there's no port selected,
-// make a port select button appear:
-function makePortButton() {
-  // create and position a port chooser button:
-  portButton = createButton("choose port");
-  portButton.position(10, 10);
-  // give the port button a mousepressed handler:
-  portButton.mousePressed(choosePort);
-}
+// function allSerialStuff() {
+//   if (!navigator.serial) {
+//     alert("WebSerial is not supported in this browser. Try Chrome or MS Edge.");
+//   }
+//   // check for any ports that are available:
+//   serial.getPorts();
+//   // if there's no port chosen, choose one:
+//   serial.on("noport", makePortButton);
+//   // open whatever port is available:
+//   serial.on("portavailable", openPort);
+//   // handle serial errors:
+//   serial.on("requesterror", portError);
+//   // handle any incoming serial data:
+//   serial.on("data", serialEvent);
+//   serial.on("close", makePortButton);
+//   // add serial connect/disconnect listeners:
+//   navigator.serial.addEventListener("connect", portConnect);
+//   navigator.serial.addEventListener("disconnect", portDisconnect);
+// }
+// // if there's no port selected,
+// // make a port select button appear:
+// function makePortButton() {
+//   // create and position a port chooser button:
+//   portButton = createButton("choose port");
+//   portButton.position(10, 10);
+//   // give the port button a mousepressed handler:
+//   portButton.mousePressed(choosePort);
+// }
 
-// make the port selector window appear:
-function choosePort() {
-  if (portButton) portButton.show();
-  serial.requestPort();
-}
+// // make the port selector window appear:
+// function choosePort() {
+//   if (portButton) portButton.show();
+//   serial.requestPort();
+// }
 
-// open the selected port, and make the port
-// button invisible:
-// open the selected port, and make the port
-// button invisible:
-function openPort() {
-  // wait for the serial.open promise to return,
-  // then call the initiateSerial function
-  serial.open().then(initiateSerial);
+// // open the selected port, and make the port
+// // button invisible:
+// // open the selected port, and make the port
+// // button invisible:
+// function openPort() {
+//   // wait for the serial.open promise to return,
+//   // then call the initiateSerial function
+//   serial.open().then(initiateSerial);
 
-  // once the port opens, let the user know:
-  function initiateSerial() {
-    console.log("port open");
-  }
-  // hide the port button once a port is chosen:
-  if (portButton) portButton.hide();
-}
+//   // once the port opens, let the user know:
+//   function initiateSerial() {
+//     console.log("port open");
+//   }
+//   // hide the port button once a port is chosen:
+//   if (portButton) portButton.hide();
+// }
 
-// pop up an alert if there's a port error:
-function portError(err) {
-  alert("Serial port error: " + err);
-}
-// read any incoming data as a string
-// (assumes a newline at the end of it):
+// // pop up an alert if there's a port error:
+// function portError(err) {
+//   alert("Serial port error: " + err);
+// }
+// // read any incoming data as a string
+// // (assumes a newline at the end of it):
 
-// try to connect if a new serial port
-// gets added (i.e. plugged in via USB):
-function portConnect() {
-  console.log("port connected");
-  serial.getPorts();
-}
+// // try to connect if a new serial port
+// // gets added (i.e. plugged in via USB):
+// function portConnect() {
+//   console.log("port connected");
+//   serial.getPorts();
+// }
 
-// if a port is disconnected:
-function portDisconnect() {
-  serial.close();
-  console.log("port disconnected");
-}
+// // if a port is disconnected:
+// function portDisconnect() {
+//   serial.close();
+//   console.log("port disconnected");
+// }
 
-function closePort() {
-  serial.close();
-}
+// function closePort() {
+//   serial.close();
+// }
